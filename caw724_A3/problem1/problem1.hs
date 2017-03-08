5ba21151046f34652c4c7bccc3d4fd4222764dfd
Chad A. Woitas <woitas.chad@gmail.com>
2017-03-08 12:01:53 -0600

    finished a3
    uploaded test log

--- caw724_A3/problem1/problem1.hs
+++ caw724_A3/problem1/problem1.hs
@@ -5,28 +5,55 @@
 --caw724
 --11137533
 
-
+--apologies for not seperating this into folders
 
 --Problem 1)
 unfold :: (a -> Bool) -> (a -> b) -> (a -> a) -> a -> [b] 
 unfold p h t x  | p x = []
         | otherwise  = h x : unfold p h t (t x)
 
+myMap :: (a->b) -> [a] -> [b]
+myMap f = unfold null (f.head) tail
 
---myMap f x:xs = unfold (==[]) f tail x
-
-iterate f = unfold (==False) f id
+iterate :: (a->a) -> a -> [a]
+iterate f = unfold (const False) f f
 
 -- fcn id and . may be useful
 
 --Problem 2)
+altmap :: (a->b) -> (a->b) -> [a] -> [b]
 altmap _ _ [] = [] 
 altmap f g (x:xs) = f x : (altmap g f xs)
 
 --Problem 3)
+luhnDouble :: (Num a)=>[a] -> [a]
 luhnDouble (x:xs) = altmap id (*2) (reverse (x:xs))
+
 
 luhnF [] = []
 luhnF (x:xs) = (if x>9 then x-9 else x) : luhnF xs
 
 luhn list = ((sum (luhnF (luhnDouble list))) `mod` 10) == 0
+
+
+ 
+--Problem 4
+--[(x, y) | x <- [1, 2, 3], y <- [4, 5, 6]] 
+--Make that^ with one generator
+
+allpairs :: [a] -> [b] -> [(a,b)]
+allpairs [] _ = []
+allpairs _ [] = []
+allpairs (x:xs) (y:ys) = (x,y) : (allpairs xs (y:ys))
+
+--Problem 5
+
+perfect = map factorSum [1..]
+
+factorSum num = if ((sum (factorCheck num [1 .. (num-1)])) == num)
+        then num:factorSum (num+1)
+                else factorSum (num+1)
+
+factorCheck _ [] = []
+factorCheck num (x:xs) = (if (num `mod` (x) == 0) then x else 0): (factorCheck num xs)
+
